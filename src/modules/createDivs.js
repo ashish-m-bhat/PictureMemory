b958e3bbfef3cd73e73baab96725b697f9566766
Ashish M Bhat <ashish.m.bhat98@gmail.com>
2022-07-11 18:44:49 +0530

    Create Tiles dynamically

--- src/modules/createDivs.js
+++ src/modules/createDivs.js
@@ -1,13 +1,19 @@
-const createDivs = () =>{
-    // Array to store the ids. 2 divs will share the same class names but all will have unique ids
-    const idArray = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve']
-
+const createDivs = tilesArray =>{
     const wrapperDiv = document.querySelector('#wrapperDiv');
-    for(let i=1;i<=idArray.length;i++){
+    for(let i=1;i<=tilesArray.length;i++){
       const innerDiv = document.createElement('div');
-      innerDiv.setAttribute('id', idArray[i-1]);
-      innerDiv.setAttribute('class', Math.ceil(i/2).toString());
-      innerDiv.style.cssText="background-color:#67cdfc;height:25%;width:10%;margin:6vw;margin-top:2vh;margin-bottom:2vh;cursor:pointer;box-shadow: #005780 0px 22px 70px 4px;";
+      innerDiv.setAttribute('id', tilesArray[i-1].id);
+      innerDiv.setAttribute('class', tilesArray[i-1].class);
+      innerDiv.style.cssText=`
+                        background-color:#67cdfc;
+                        height:25%;
+                        width:10%;
+                        margin:6vw;
+                        margin-top:2vh;
+                        margin-bottom:2vh;
+                        cursor:pointer;
+                        box-shadow: #005780 0px 22px 70px 4px;
+                        border-radius:10%`;
       wrapperDiv.append(innerDiv);
     }
   }
