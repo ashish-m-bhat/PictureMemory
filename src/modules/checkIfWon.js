47c98189435455743103403ec68395fa125532d0
Ashish M Bhat <ashish.m.bhat98@gmail.com>
2022-07-11 10:11:31 +0530

    Check If won

---
+++ src/modules/checkIfWon.js
@@ -0,0 +1,9 @@
+export default function checkIfWon(currentStack, countOfTries){
+    // If stack length is 12, game over
+    // All the tils are in the stack now
+    if(currentStack.length === 12){
+        setTimeout(()=>{
+          alert(`You've won! with ${countOfTries} tries`);
+          location.reload();
+        },500)}
+}
